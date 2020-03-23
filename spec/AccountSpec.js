9b1926150673a75646e16fe68ae292c4a6b89a16
Joe Ellingworth <joeellingworth@gmail.com>
2020-03-23 14:50:50 +0000

    Added second test

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -1,9 +1,15 @@
 describe("Account", function() {
 
+  it("can deposit into account", function(){
+    var account = new Account();
+    account.deposit(5.00)
+    expect(account.balance).toEqual(5.00)
+  }) 
+
   it("Should print the correct balance for in deposit", function() {
     var account = new Account();
     account.deposit(500.00, "01/03/2020")
     expect(account.printStatment()).toEqual("date || credit || debit || balance /n 01/03/2020 || 500.00 || || 500.00")
   })
-  
+
 })
