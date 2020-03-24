535cb7f8a5c8e9ca8b99540bb455222f47f6435f
Joe Ellingworth <joeellingworth@gmail.com>
2020-03-24 14:12:41 +0000

    tests passing after refactor

--- spec/PrintStatementSpec.js
+++ spec/PrintStatementSpec.js
@@ -6,14 +6,14 @@ describe("PrintStatement", function() {
 
   it("Should print the correct balance for in deposit", function() {
     account.transactions = [[" 01/03/2020 || 500.00 || || 500.00"]]
-    printStatment = new PrintStatment(account.transactions)
-    expect(printStatment.statement()).toEqual("date || credit || debit || balance\n 01/03/2020 || 500.00 || || 500.00\n")
+    printStatement = new PrintStatement(account.transactions)
+    expect(printStatement.statement()).toEqual("date || credit || debit || balance\n 01/03/2020 || 500.00 || || 500.00\n")
   })
 
   it("Should print the correct statement from after and deposit and withdrawl", function() {
     account.transactions = [[" 01/03/2020 || 1000.00 || || 1000.00"],[" 02/03/2020 || || 500.00 || 500.00"]]
-    printStatment = new PrintStatment(account.transactions)
-    expect(printStatment.statement()).toEqual("date || credit || debit || balance\n 02/03/2020 || || 500.00 || 500.00\n 01/03/2020 || 1000.00 || || 1000.00\n")
+    printStatement = new PrintStatement(account.transactions)
+    expect(printStatement.statement()).toEqual("date || credit || debit || balance\n 02/03/2020 || || 500.00 || 500.00\n 01/03/2020 || 1000.00 || || 1000.00\n")
   })
 
 })
--- src/PrintStatement.js
+++ src/PrintStatement.js
@@ -1,13 +1,31 @@
-function PrintStatment(accountHistory) {
+function PrintStatement(accountHistory) {
   this.history = accountHistory
   this.statement
 }
 
-PrintStatment.prototype.statement = function() {
-  var statement = "date || credit || debit || balance\n"
+PrintStatement.prototype.statement = function() {
+  return this.printHeaders() + this.printHistory()
+}
+
+PrintStatement.prototype.printHistory = function() {
+  var statement = ""
   var i;
   for (i = this.history.length - 1; i >= 0 ; i--) {
     statement +=  (this.history[i] + "\n")
   };
+  console.log(statement)
   return statement
 }
+
+PrintStatement.prototype.printHeaders = function() {
+  const headers = 'date || credit || debit || balance\n';
+  console.log(headers);
+  return headers;
+};
+
+
+'date || credit || debit || balance 02/03/2020 || || 500.00 || 500.00 01/03/2020 || 1000.00 || || 1000.00'
+'date || credit || debit || balance 02/03/2020 || || 500.00 || 500.00 01/03/2020 || 1000.00 || || 1000.00'
+
+'date || credit || debit || balance 01/03/2020 || 500.00 || || 500.00'
+'date || credit || debit || balance 01/03/2020 || 500.00 || || 500.00'
